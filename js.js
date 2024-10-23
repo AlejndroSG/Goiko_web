882d0d9ffea4fec4aa2156437d8778b6b7170a5d
AlejndroSG <alex2005sg@gmail.com>
2024-10-23 13:31:04 +0200

    Arreglo de scroll en menu desplegable, insertamos animación en banner y modificamos la de ofertas

--- js.js
+++ js.js
@@ -5,14 +5,17 @@ document.addEventListener("DOMContentLoaded", ()=>{
     let enlaces = document.querySelectorAll(".menu .opciones a");
     let slider = document.querySelector("section#slider");
     let sliderdivs = document.querySelectorAll("section#slider div");
+    let header = document.querySelector("header");
 
     burger.addEventListener("click", () =>{
         desplegable.style.height = "100%";
         desplegable.style.backgroundColor = "#EE2737";
+        document.body.style.overflow = "hidden";
     })
 
     cross.addEventListener("click", () =>{
         desplegable.style.height = "0%";
+        document.body.style.overflow = "visible";
     })
 
     enlaces.forEach(enlace => {
@@ -28,30 +31,36 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     window.addEventListener("scroll", () =>{
         if(window.scrollY > slider.offsetTop - window.innerHeight/4){
-            slider.classList.add("tam");
             sliderdivs.forEach(div => {
                 div.style.opacity = "1";
             })
+            slider.style.borderRight = "0";
+            slider.style.borderLeft = "0";
+            // header.style.opacity = "0";
         }else{
-            slider.classList.remove("tam");
             sliderdivs.forEach(div => {
                 div.style.opacity = "0";
             })
+            slider.style.border = "2px solid white";
+            // header.style.opacity = "1";
         }
-        if(window.scrollY > slider.offsetTop - window.innerHeight/3){
-            slider.classList.add("tam2");
-        }else{
-            slider.classList.remove("tam2");
-        }
-        if(window.scrollY > slider.offsetTop - window.innerHeight/2){
-            slider.classList.add("tam3");
-        }else{
-            slider.classList.remove("tam3");
-        }
-        if(window.scrollY > slider.offsetTop - window.innerHeight/1.5){
-            slider.classList.add("tam4");
-        }else{
-            slider.classList.remove("tam4");
-        }
+
+        // if(window.scrollY > slider.offsetTop - window.innerHeight/3){
+        //     header.style.opacity = "0.33";
+        // }else{
+        //     header.style.opacity = "0.66";
+        // }
+
+        // if(window.scrollY > slider.offsetTop - window.innerHeight/2){
+        //     header.style.opacity = "0.66";
+        // }else{
+        //     header.style.opacity = "0.33";
+        // }
+
+        // if(window.scrollY > slider.offsetTop - window.innerHeight){
+        //     header.style.opacity = "1";
+        // }else{
+        //     header.style.opacity = "0";
+        // }
     })
 })
